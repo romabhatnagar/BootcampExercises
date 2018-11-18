5bb20a011988edda1d2cd47dc1fdaeaf59fefeb8
romabhatnagar <roma.bhatnagar@tothenew.com>
2018-11-18 12:52:30 +0530

    404 and 500 added

--- src/main/java/com/ttn/springboot/reapdemo/ReapDemoApplication.java
+++ src/main/java/com/ttn/springboot/reapdemo/ReapDemoApplication.java
@@ -3,6 +3,17 @@ package com.ttn.springboot.reapdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 /*import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,50 +23,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;*/
 
+import javax.servlet.Registration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.sql.DataSource;
 
 @SpringBootApplication
-public class ReapDemoApplication {
-//    extends
-//} WebSecurityConfigurerAdapter {
-//   @Autowired
-//    DataSource dataSource;
-
-   /*@Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }*/
-
-    // Enable jdbc authentication
-   /* @Autowired
-    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder());
-    }*/
-
-  /* @Bean
-    public JdbcUserDetailsManager jdbcUserDetailsManager() throws Exception {
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
-        jdbcUserDetailsManager.setDataSource(dataSource);
-        return jdbcUserDetailsManager;
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/signUp", "/forgotPassword", "/login" +
-                "").permitAll().antMatchers("/")
-                .hasAnyRole("USER", "ADMIN", "").antMatchers("/getEmployees").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/login").hasAnyRole("ADMIN").anyRequest().authenticated().and().formLogin()
-                .loginPage("/login").permitAll().and().logout().permitAll();
-
-        http.csrf().disable();
-    }*/
+//@ComponentScan(basePackages ="com.ttn.springboot.reapdemo.controller")
+public class ReapDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ReapDemoApplication.class, args);
     }
+
 }
+
